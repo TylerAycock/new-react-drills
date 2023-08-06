92bdcdf14b4ea760be1419c2c3001779a11bdb6e
Tyler Aycock <tyler.joseph.aycock@gmail.com>
2023-08-06 10:31:34 -0700

    completed 4th challenge

--- app-4/src/components/Form.js
+++ app-4/src/components/Form.js
@@ -1,37 +1,19 @@
-import './Form.css'
-import {useState} from 'react'
-
+import { useState } from 'react'
+import "./Form.css"
 const Form = () => {
-
-    const [username, setUsername] = useState('')
+    const [userName, setUserName] = useState('')
     const [password, setPassword] = useState('')
 
-    const handleSubmit = e =>{
-        e.preventDefault()
-        alert(`Username: ${username} & Password ${password}`)
-        setUsername('')
-        setPassword('')
+    const clickHandler = () => {
+        alert(`Username: ${userName} PassWord: ${password}`)
     }
 
-    const usernameHandler = e =>{
-        setUsername(e.target.value)
-    }
-
-    const passwordHandler = e =>{
-        setPassword(e.target.value)
-    }
-    return(
-        <form onSubmit={handleSubmit}>
-            <div className='input'>
-            <label htmlFor="username">User Name:</label>
-            <input type="text" name="username" id="username" onChange={usernameHandler} value={username}/>
-            </div>
-            <div className='input'>
-            <label htmlFor="password">Password:</label>
-            <input type="text" name="password" id="password" onChange={passwordHandler} value={password}/>
-            </div>
-            <button type='submit'>Submit</button>
-        </form>
+    return (
+        <div className='login'>
+            <input type="text" placeholder="username" name="userName" onChange={e => { setUserName(e.target.value) }} />
+            <input type="text" placeholder="password" name="password" onChange={e => { setPassword(e.target.value) }} />
+            <button onClick={clickHandler} className='btn'>Submit</button>
+        </div>
     )
 }
 
