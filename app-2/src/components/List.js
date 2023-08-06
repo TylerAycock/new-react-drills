44820a5db6068d6bf438b4b7755b00dbc591ebf7
Tyler Aycock <tyler.joseph.aycock@gmail.com>
2023-08-06 10:30:35 -0700

    completed 2nd challenge

--- app-2/src/components/List.js
+++ app-2/src/components/List.js
@@ -1,18 +1,22 @@
-import { useState } from "react";
-import "./List.css";
+import {useState} from 'react'
+import './List.css'
 
-const List = () => {
-  const [list, setList] = useState([
-    "spaghetti",
-    "lasagna",
-    "ice cream",
-    "sushi",
-    "bologna",
-    "cheese",
-  ]);
-  return list.map((item) => {
-    return <p className="item">{item.toUpperCase()}</p>;
-  });
-};
+const List = ()=> {
 
-export default List;
+  const [list, setList] = useState(['pasta', 'pasta', 'ice cream', 'meat loaf'])
+
+  const items = list.map(food=> {
+    return(
+      <h2>{food}</h2>
+    )
+  })
+  return(
+    <div className='list'>
+      {items}
+    </div>
+  )
+
+
+}
+
+export default List 
